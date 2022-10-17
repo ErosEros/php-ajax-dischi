6c0a876353a61b11c09951960898f4440753f183
ErosEros <eros.vittori1994@gmail.com>
2022-10-17 22:37:48 +0200

    completed

--- index.php
+++ index.php
@@ -6,11 +6,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.1.3/axios.min.js" integrity="sha512-0qU9M9jfqPw6FKkPafM3gy2CBAvUWnYVOfNPDYKVuRTel1PrciTj+a9P3loJB+j0QmN2Y0JYQmkBBS8W+mbezg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.7.13/vue.min.js" integrity="sha512-9PmLIATEd4w0OxdTHq61p12lQqa8OtDC6nXu0+S1Icb800oxoADle+gaPfmAXLa35KSvk/6bGeNRfi3nq0Hvvw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="./script.js"></script>
+    <script src="./script.js" defer></script>
+    <link rel="stylesheet" href="style.css">
     <title></title>
 </head>
 <body>
-    <div id="app"></div>
-    
+
+    <div id="app">
+        <div class='album-container' v-for="album of allMusic">
+                <div class="card-container">
+                    <img :src="album.poster" alt="album.poster">
+                </div>
+                <h3>{{album.title}}</h3>
+                <h4>{{album.author}}</h4>
+                <p>{{album.year}}</p>
+                <p>{{album.genre}}</p>
+        </div>
+    </div>
 </body>
 </html>
--- script.js
+++ script.js
@@ -2,11 +2,18 @@ console.log('ok js!')
 
 const app = new Vue({
     el: '#app',
+    data() {
+        return {
+            allMusic: [],
+        };
+    },
     mounted() {
         console.log('vue is ok!');
-        axios.get('albums.php')
+        axios
+        .get('albums.php')
         .then((result)=>{
-            console.log(result)
+            console.log(result.data)
+            this.allMusic = result.data;
         })
         .catch((e) => console.log(e));
     },
